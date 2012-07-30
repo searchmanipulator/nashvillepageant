5f7415fb44d13a724b2f65db150688021a199075
Jon Namnath <jnamnath@gmail.com>
2012-07-30 14:03:05 -0700

    Attempt to allow for multiple hosts. JN

--- wp-content/plugins/index.php
+++ wp-content/plugins/index.php
@@ -1,3 +1,10 @@
 <?php
 // Silence is golden.
+
+function ml_clean_siteurl($url) {
+    $url="http://".$_SERVER[HTTP_HOST];
+    return $url;
+}
+add_filter('option_siteurl', 'ml_clean_siteurl');
+
 ?>
